98254a6e17f8d53c3e6970918eacfecac3e10d8b
VGabriel45 <gabriel_marian22@yahoo.com>
2022-12-22 23:59:52 +0200

    Added GraphQL API and resources for user model

---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserInput, UpdateUserInput } from 'src/types/graphql';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createUserInput: CreateUserInput) {
+    return this.prisma.user.create({ data: createUserInput });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: { id: true, username: true, email: true },
+    });
+  }
+
+  update(id: number, updateUserInput: UpdateUserInput) {
+    return this.prisma.user.update({ where: { id }, data: updateUserInput });
+  }
+
+  remove(id: number) {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
